b8a4cac26c50876ee7caa754236cca77c2a34d66
Brian Herard <bmherard@gmail.com>
2021-07-28 11:54:13 -0400

    Initial Commit

---
+++ themes/simplesite/static/js/back-to-top.js
@@ -0,0 +1,18 @@
+var mybutton = document.getElementById("myBtn");
+
+// when user scrolls down 300px from top of page, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// when user clicks on the button, scroll to top of pages
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
